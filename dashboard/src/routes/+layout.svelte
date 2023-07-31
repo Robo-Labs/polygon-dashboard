3afe5b534b2d4f093adb2d453132b6530b35dbcf
hazelnutcloud <hsnmkls@proton.me>
2023-07-31 09:42:22 +0800

    add font + seo stuff

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -8,6 +8,35 @@
 	export let data: LayoutData;
 </script>
 
+<svelte:head>
+	<title>DeFi Lender Dashboard | Robo Labs</title>
+	<meta name="description" content="Analytics for DeFi lending protocols by the Robo Labs team" />
+	<meta name="keywords" content="DeFi Lender Dashboard | Robo Labs" />
+	<meta name="author" content="Robo Labs" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="utf-8" />
+	<meta name="theme-color" content="#000000" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@robolabs_biz" />
+	<meta name="twitter:creator" content="@robolabs_biz" />
+	<meta name="twitter:title" content="DeFi Lender Dashboard | Robo Labs" />
+	<meta
+		name="twitter:description"
+		content="Analytics for DeFi lending protocols by the Robo Labs team"
+	/>
+	<meta name="twitter:image" content="robolabs_og.png" />
+	<meta property="og:title" content="DeFi Lender Dashboard | Robo Labs" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://robolabs.biz" />
+	<meta property="og:image" content="robolabs_og.png" />
+	<meta
+		property="og:description"
+		content="Analytics for DeFi lending protocols by the Robo Labs team"
+	/>
+	<meta property="og:site_name" content="DeFi Lender Dashboard | Robo Labs" />
+	<meta property="og:locale" content="en_US" />
+</svelte:head>
+
 <QueryClientProvider client={data.queryClient}>
 	<div class="w-full">
 		<Header />
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
-		// fontFamily: {
-		// 	'header': ['Roboto', 'sans-serif'],
-		// }
+		extend: {
+			fontFamily: {
+				'header': ['Roboto', ...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
